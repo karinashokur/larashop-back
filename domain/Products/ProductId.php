ad53de663ebb0fb02450a62a4bc8b59134bd6c97
Karina <karina.shokur@devit-team.com>
2021-05-06 21:25:59 +0000

    Create ProductId value object

---
+++ domain/Products/ProductId.php
@@ -0,0 +1,19 @@
+<?php
+namespace Domain\Products;
+use Ramsey\Uuid\Uuid;
+final class ProductId
+{
+    private $uuid;
+    public function __construct(Uuid $uuid)
+    {
+        $this->uuid = $uuid;
+    }
+    public static function generate(): self
+    {
+        return new self(Uuid::uuid4());
+    }
+    public function toString(): string
+    {
+        return $this->uuid->toString();
+    }
+}
