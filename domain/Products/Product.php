009710c54bcc58a158faebaf1ab51ea6374a4f8e
Karina <karina.shokur@devit-team.com>
2021-05-10 07:53:53 +0000

    Allow ProductId to be set on Product instance

--- domain/Products/Product.php
+++ domain/Products/Product.php
@@ -3,4 +3,8 @@ namespace Domain\Products;
 use Illuminate\Database\Eloquent\Model;
 final class Product extends Model
 {
+    public function setId(ProductId $productId): void
+    {
+        $this->uuid = $productId->toString();
+    }
 }
