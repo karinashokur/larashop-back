8672c72955b0f55127c2541167e80e0aa2fe00a8
Karina <karina.shokur@devit-team.com>
2021-06-17 15:02:47 +0000

    Provide factory to create Products to be used in tests

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,11 @@
+<?php
+use Domain\Products\Product;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+$factory->define(Product::class, function (Faker $faker) {
+    return [
+        'uuid' => $faker->uuid,
+        'name' => Str::random(30),
+        'price' => $faker->randomFloat(2, 1, 1000),
+    ];
+});
